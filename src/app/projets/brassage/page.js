a03456fbfa55e73a0de63c6b840d5e7307951811
WilliamBerne95 <william.berne@outlook.com>
2024-03-14 22:18:39 +0100

    maj site 1403

---
+++ src/app/projets/brassage/page.js
@@ -0,0 +1,15 @@
+// /src/app/projets/Portfolio/page.js
+import React from "react";
+
+const brassage = () => {
+    return (
+        <div className="container mx-auto py-12">
+            <h1 className="text-4xl font-bold mb-8">Mon projet Portfolio</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                {/* Le reste du contenu reste inchangé */}
+            </div>
+        </div>
+    );
+};
+
+export default brassage;
