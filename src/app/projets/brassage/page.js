a934da4977bdaf430d0cb832fed4b2d10ad57274
WilliamBerne95 <william.berne@outlook.com>
2024-03-15 00:04:42 +0100

    bug8

--- src/app/projets/brassage/page.js
+++ src/app/projets/brassage/page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from "next/image";
 
 const images = [
     {
@@ -27,11 +28,11 @@ const images = [
 const Brassage = () => {
     return (
         <div className="container mx-auto py-12">
-            <h1 className="text-4xl font-bold mb-8">Montage d'une baie de brassage</h1>
+            <h1 className="text-4xl font-bold mb-8">{`Montage d'une baie de brassage`}</h1>
 
             <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-4">Description</h2>
-                <p>Gaming Explorer est un projet basé sur Next.JS qui propose un site permettant aux utilisateurs de rechercher des jeux vidéo via l'API de Rawg.io. L'application offre des fonctionnalités avancées telles qu'un compte utilisateur permettant d'ajouter des jeux aux favoris et bien plus encore.</p>
+                <p>{`Description`}</p>
             </section>
 
             <section className="mb-8">
@@ -40,11 +41,11 @@ const Brassage = () => {
             </section>
 
             <section className="mb-8">
-                <h2 className="text-2xl font-bold mb-4">Galerie d'images</h2>
+                <h2 className="text-2xl font-bold mb-4">{`Galerie d'images`}</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                     {images.map((image, index) => (
                         <div key={index} className="mb-4">
-                            <img src={image.original} alt={image.description} className="w-full h-auto" />
+                            <Image src={image.original} alt={image.description} className="w-full h-auto" />
                             <p className="text-center mt-2">{image.description}</p>
                         </div>
                     ))}
