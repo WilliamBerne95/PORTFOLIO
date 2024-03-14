4f25ced37dbdbd57a01aea6210a3b212caffc750
WilliamBerne95 <william.berne@outlook.com>
2024-03-14 23:17:37 +0100

    bug4

--- src/app/projets/vpn/page.js
+++ src/app/projets/vpn/page.js
@@ -20,28 +20,27 @@ const images = [
     },
 ];
 
-const vpn = () => {
-
+const Vpn = () => {
     return (
         <div className="container mx-auto py-12">
-            <h1 className="text-4xl font-bold mb-8">{`Configuration VPN`}</h1>
+            <h1 className="text-4xl font-bold mb-8">Configuration VPN</h1>
 
             <section className="mb-8">
                 <h2 className="text-2xl font-bold mb-4">Description</h2>
                 <p>
-                    {`Le processus de configuration VPN implique la sélection des certificats
-                        appropriés et la configuration des paramètres réseau pour établir une
-                        connexion sécurisée.`}
+                    Le processus de configuration VPN implique la sélection des certificats
+                    appropriés et la configuration des paramètres réseau pour établir une
+                    connexion sécurisée.
                 </p>
             </section>
 
             <section className="mb-8">
-                <h2 className="text-2xl font-bold mb-4">{`Durée du projet`}</h2>
+                <h2 className="text-2xl font-bold mb-4">Durée du projet</h2>
                 <p>Mai - Septembre 2023</p>
             </section>
 
             <section className="mb-8">
-                <h2 className="text-2xl font-bold mb-4">{`Galerie d'images`}</h2>
+                <h2 className="text-2xl font-bold mb-4">Galerie d'images</h2>
             </section>
 
             <Link href="/projets" legacyBehavior>
@@ -52,4 +51,4 @@ const vpn = () => {
     );
 };
 
-export default vpn;
+export default Vpn;
