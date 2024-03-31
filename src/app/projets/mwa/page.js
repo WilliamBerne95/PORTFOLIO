18d06fb36fa82661b1c2ea0c34cd50e899e474ef
WilliamBerne95 <william.berne@outlook.com>
2024-03-31 11:20:37 +0200

    upgrades zoom

--- src/app/projets/mwa/page.js
+++ src/app/projets/mwa/page.js
@@ -1,8 +1,8 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-// Define your image data for the Motorized Wide Area project here
 const motorizedWideAreaData = [
     {
         path: '/images/projects/mwa/cahier-des-charges.png',
@@ -47,6 +47,12 @@ const motorizedWideAreaData = [
 ];
 
 const MotorizedWideArea = () => {
+    const [zoomedItem, setZoomedItem] = useState(null);
+
+    const toggleZoom = (item) => {
+        setZoomedItem(zoomedItem === item ? null : item);
+    };
+
     return (
         <div className="container mx-auto py-12 px-4">
             <div className="text-center mb-12">
@@ -56,13 +62,24 @@ const MotorizedWideArea = () => {
 
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {motorizedWideAreaData.map((item, index) => (
-                    <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden p-4">
-                        <Image src={item.path} alt={item.description} width={500} height={300} layout="responsive" />
+                    <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden p-4 cursor-pointer" onClick={() => toggleZoom(item)}>
+                        <Image src={item.path} alt={item.description} width={500} height={300} layout="responsive"/>
                         <h3 className="text-lg font-bold my-2">{item.description}</h3>
                         <p className="text-gray-700">{item.comment}</p>
                     </div>
                 ))}
             </div>
+
+            {zoomedItem && (
+                <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50 p-4" onClick={() => setZoomedItem(null)}>
+                    <div className="bg-white p-4 max-w-3xl max-h-full overflow-auto" onClick={e => e.stopPropagation()}>
+                        <Image src={zoomedItem.path} alt={zoomedItem.description} width={800} height={450} layout="responsive"/>
+                        <h3 className="text-lg font-bold my-2">{zoomedItem.description}</h3>
+                        <p className="text-gray-700">{zoomedItem.comment}</p>
+                    </div>
+                </div>
+            )}
+
             <div className="text-center mt-12">
                 <Link legacyBehavior={true} href="/projets">
                     <a className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors">Retour aux projets</a>
