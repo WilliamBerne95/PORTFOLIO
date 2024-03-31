fc018fab8c985419998b0981ca1a45dc3aee746a
WilliamBerne95 <william.berne@outlook.com>
2024-04-01 01:45:18 +0200

    save

--- src/app/projets/arrivants/page.js
+++ src/app/projets/arrivants/page.js
@@ -1,99 +1,170 @@
-import React from 'react';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
 
 const images = [
     {
-        original: '/images/projects/newarrivants/AD.PNG',
+        path: '/images/projects/newarrivants/AD.PNG',
         description: 'Utilisateurs Active Directory',
         comment: 'Gestion des comptes utilisateurs et des permissions dans Active Directory.'
     },
     {
-        original: '/images/projects/newarrivants/arboicasso.jpg',
+        path: '/images/projects/newarrivants/arboicasso.jpg',
         description: 'Arborescence des associations',
         comment: 'Visualisation de la structure des associations dans le système.'
     },
     {
-        original: '/images/projects/newarrivants/BALF1.PNG',
+        path: '/images/projects/newarrivants/BALF1.PNG',
         description: 'Boîtes aux lettres fonctionnelles',
         comment: 'Configuration des boîtes aux lettres fonctionnelles pour les utilisateurs.'
     },
     {
-        original: '/images/projects/newarrivants/BALF2.PNG',
+        path: '/images/projects/newarrivants/BALF2.PNG',
         description: 'Détails des boîtes aux lettres',
         comment: 'Affichage des propriétés spécifiques des boîtes aux lettres fonctionnelles.'
     },
     {
-        original: '/images/projects/newarrivants/Creaboite mailOBM.PNG',
+        path: '/images/projects/newarrivants/Creaboite mailOBM.PNG',
         description: 'Création d’une boîte mail OBM',
         comment: 'Procédure de création d’une nouvelle boîte mail dans OBM.'
     },
     {
-        original: '/images/projects/newarrivants/CreationprofilGLPI.PNG',
+        path: '/images/projects/newarrivants/CreationprofilGLPI.PNG',
         description: 'Création de profil dans GLPI',
         comment: 'Étape de création d’un profil utilisateur dans l’outil GLPI.'
     },
     {
-        original: '/images/projects/newarrivants/droitsAD.png',
+        path: '/images/projects/newarrivants/droitsAD.png',
         description: 'Droits d’accès Active Directory',
         comment: 'Configuration des droits d’accès pour les répertoires de travail sur le serveur de fichiers.'
     },
     {
-        original: '/images/projects/newarrivants/fichemob1.jpg',
+        path: '/images/projects/newarrivants/fichemob1.jpg',
         description: 'Fiche de mouvement du personnel - Page 1',
         comment: 'Première page du formulaire de mouvement du personnel.'
     },
     {
-        original: '/images/projects/newarrivants/fichemob2.jpg',
+        path: '/images/projects/newarrivants/fichemob2.jpg',
         description: 'Fiche de mouvement du personnel - Page 2',
         comment: 'Seconde page du formulaire de mouvement du personnel, détaillant les moyens informatiques demandés.'
     },
     {
-        original: '/images/projects/newarrivants/fichemob3.jpg',
+        path: '/images/projects/newarrivants/fichemob3.jpg',
         description: 'Fiche de mouvement du personnel - Page 3',
         comment: 'Troisième page du formulaire de mouvement du personnel, incluant les droits d’accès.'
     },
     {
-        original: '/images/projects/newarrivants/GLPIticketnouvelarrivant3.PNG',
+        path: '/images/projects/newarrivants/GLPIticketnouvelarrivant3.PNG',
         description: 'Suivi de ticket GLPI pour un nouvel arrivant',
         comment: 'Suivi des étapes de traitement d’un ticket pour un nouvel arrivant dans GLPI.'
     },
     {
-        original: '/images/projects/newarrivants/NOUVEL-ARRIVANTv4.jpg',
+        path: '/images/projects/newarrivants/NOUVEL-ARRIVANTv4.jpg',
         description: 'Procédure pour un nouvel arrivant',
         comment: 'Document détaillant les étapes de configuration pour un nouvel arrivant.'
     },
     {
-        original: '/images/projects/newarrivants/profilAD.png',
+        path: '/images/projects/newarrivants/profilAD.png',
         description: 'Propriétés de profil dans Active Directory',
         comment: 'Modification des propriétés d’un utilisateur dans Active Directory.'
     }
 ];
 
 const NouveauxArrivants = () => {
+    const [zoomedItem, setZoomedItem] = useState(null);
+    const [animation, setAnimation] = useState('zoomIn');
+
+    const closeZoom = () => {
+        setAnimation('zoomOut');
+        setTimeout(() => {
+            setZoomedItem(null);
+        }, 300);
+    };
+
+    useEffect(() => {
+        if (zoomedItem) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    }, [zoomedItem]);
+
     return (
         <div className="container mx-auto py-12 px-4">
             <div className="text-center mb-12">
-                <h1 className="text-4xl font-bold mb-4">Accueil des Nouveaux Arrivants</h1>
-                <p className="text-xl">{`Documentation et procédures d'intégration des nouveaux membres du personnel.`}</p>
+                <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-secondary-500">Accueil
+                    des Nouveaux Arrivants</h1>
+                <p className="text-lg sm:text-xl md:text-2xl text-secondary-300">{`Documentation et procédures d'intégration des nouveaux membres du personnel.`}</p>
             </div>
 
-            <div className="mb-8">
-                <h2 className="text-3xl font-bold underline decoration-wavy mb-6">{`Processus d'Intégration`}</h2>
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {images.map((image, index) => (
-                        <div key={index} className="bg-white rounded-lg shadow-lg overflow-hidden p-4">
-                            <Image src={image.original} alt={image.description} width={500} height={300} layout="responsive" />
-                            <h3 className="text-lg font-bold my-2">{image.description}</h3>
-                            <p className="text-gray-700">{image.comment}</p>
-                        </div>
-                    ))}
-                </div>
+            <div
+                className="bg-gray-900 text-secondary-300 font-mono text-base sm:text-lg md:text-xl overflow-y-scroll p-6 my-6"
+                style={{height: '45vh'}}>
+                <Typewriter
+                    onInit={(typewriter) => {
+                        typewriter
+                            .changeDelay(10)
+                            .typeString('<span class="text-primary-500 text-3xl">Responsable du projet :</span><br>BERNE William et le reste de l\'équipe SIDSIC<br><br>')
+                            .typeString('<span class="text-primary-500 text-3xl">Description :</span><br>')
+                            .typeString("L'accueil des nouveaux arrivants consiste à préparer et à intégrer efficacement les nouveaux membres du personnel au sein de l'organisation. Cela inclut la création de leurs comptes utilisateurs, l'assignation des permissions nécessaires, la configuration de leur espace de travail numérique, et la fourniture des informations nécessaires pour leur bonne intégration. L'objectif est d'assurer une transition fluide pour les nouveaux employés et de les rendre rapidement opérationnels.<br><br>")
+                            .typeString('<span class="text-primary-500 text-3xl">Outils et plateformes utilisés :</span><br>')
+                            .typeString("• Active Directory pour la gestion des comptes utilisateurs<br>")
+                            .typeString("• GLPI pour le suivi des demandes<br>")
+                            .typeString("• OBM pour la création des boîtes mail<br><br>")
+                            .typeString('<span class="text-primary-500 text-3xl">Date de réalisation :</span><br>10/02/2023 au 01/03/2024')
+                            .start();
+                    }}
+                />
             </div>
+
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {images.map((item, index) => (
+                    <div key={index}
+                         className="relative cellule bg-white rounded-lg shadow-lg overflow-hidden p-4 cursor-pointer h-auto md:h-96 lg:h-80 transform hover:scale-105 transition-transform duration-300"
+                         onClick={() => setZoomedItem(item)}>
+                        <Image
+                            src={item.path}
+                            alt={item.description}
+                            width={500}
+                            height={300}
+                            layout="responsive"
+                            className="object-cover object-center w-full h-full"
+                        />
+                        <div className="absolute bottom-0 left-0 w-full p-4 z-10">
+                            <h3 className="text-lg font-bold text-white"
+                                style={{textShadow: '0 0 3px black'}}>{item.description}</h3>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            {zoomedItem && (
+                <div
+                    className={`fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50 p-4 ${animation}`}
+                    onClick={closeZoom}>
+                    <div className="bg-white p-4 max-w-3xl max-h-full overflow-auto"
+                         onClick={(e) => e.stopPropagation()}
+                         style={{animation: 'zoomIn 0.3s forwards'}}>
+                        <Image
+                            src={zoomedItem.path}
+                            alt={zoomedItem.description}
+                            width={800}
+                            height={450}
+                            layout="responsive"
+                            onClick={closeZoom}
+                        />
+                        <h3 className="text-lg font-bold my-2">{zoomedItem.description}</h3>
+                        <p className="text-gray-700">{zoomedItem.comment}</p>
+                    </div>
+                </div>
+            )}
 
             <div className="text-center mt-12">
                 <Link legacyBehavior={true} href="/projets">
-                    <a className="inline-block bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors">Retour aux projets</a>
+                    <a className="inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors">Retour
+                        aux projets</a>
                 </Link>
             </div>
         </div>
--- src/app/projets/brassage/page.js
+++ src/app/projets/brassage/page.js
@@ -16,6 +16,16 @@ const images = [
         comment: 'Marquage et étiquetage des câbles pour une identification facile et une maintenance efficace.',
     },
     {
+        path: '/images/projects/brassage/baiebrassage11.jpg',
+        description: 'Baie de brassage ',
+        comment: 'Baie de brassage montée pour la salle COD (cellule de crise).',
+    },
+    {
+        path: '/images/projects/brassage/switchesbrassage12.jpg',
+        description: 'Baie de brassage et switch réseau',
+        comment: 'Baie de brassage montée pour la salle COD (cellule de crise).',
+    },
+    {
         path: '/images/projects/brassage/3eth.jpg',
         description: 'Prises Ethernet murales',
         comment: "Installation des prises Ethernet murales pour faciliter l\'accès au réseau dans différentes pièces.",
@@ -24,16 +34,6 @@ const images = [
         path: '/images/projects/brassage/4ipconfig.PNG',
         description: 'Configuration IP de Windows',
         comment: 'Configuration des paramètres IP sur un poste de travail Windows pour assurer une connectivité réseau adéquate.',
-    },
-    {
-        path: '/images/projects/brassage/baiebrassage11.jpg',
-        description: 'Baie de brassage ',
-        comment: 'Baie de brassage montée pour la salle COD (cellule de crise).',
-    },
-    {
-        path: '/images/projects/brassage/switchesbrassage12.jpg',
-        description: 'Baie de brassage',
-        comment: 'Baie de brassage montée pour la salle COD (cellule de crise).',
     },
 ];
 
@@ -60,10 +60,10 @@ const BrassageManagement = () => {
         <div className="container mx-auto py-12 px-4">
             <div className="text-center mb-12">
                 <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-secondary-500">
-                    Montage d&apos;une baie de brassage
+                    Montage d'une baie de brassage
                 </h1>
                 <p className="text-lg sm:text-xl md:text-2xl text-secondary-300">
-                    Visualisation du projet de montage et d&apos;organisation d&apos;une baie de brassage.
+                    Visualisation du projet de montage et d'organisation d'une baie de brassage.
                 </p>
             </div>
 
@@ -73,15 +73,19 @@ const BrassageManagement = () => {
                     onInit={(typewriter) => {
                         typewriter
                             .changeDelay(10)
+                            .typeString('<span class="text-primary-500 text-3xl">Tâches effectuées par :</span><br>BERNE William<br><br>')
                             .typeString('<span class="text-primary-500 text-3xl">Description :</span><br>')
-                            .typeString('Description détaillée du projet de brassage ici.<br><br>')
+                            .typeString("Le montage d'une baie de brassage consiste à installer et configurer une armoire contenant l'équipement réseau (serveurs, switches, routeurs) d'une organisation. Cela inclut la planification de l'espace, l'installation physique des équipements, le câblage structuré, et la configuration pour assurer la connectivité et la communication au sein du réseau. La mission vise à créer un système organisé, sécurisé, et facile à maintenir pour gérer le flux de données efficacement.<br><br>")
+                            .typeString('<span class="text-primary-500 text-3xl">Logiciels/ plateformes utilisés :</span><br>')
+                            .typeString("• Invite de commande pour s'assurer du bon raccordement réseau<br><br>")
+                            .typeString('<span class="text-primary-500 text-3xl">Date :</span><br>05/05/2023')
                             .start();
                     }}
                 />
             </div>
 
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {images.map((item, index) => ( // Utilisez ici le bon nom de la variable qui contient les données de votre image.
+                {images.map((item, index) => (
                     <div key={index}
                          className="relative cellule bg-white rounded-lg shadow-lg overflow-hidden p-4 cursor-pointer h-auto md:h-96 lg:h-80 transform hover:scale-105 transition-transform duration-300"
                          onClick={() => setZoomedItem(item)}>
@@ -92,7 +96,6 @@ const BrassageManagement = () => {
                             height={300}
                             layout="responsive"
                             className="object-cover object-center w-full h-full"
-                            onClick={closeZoom}
                         />
                         <div className="absolute bottom-0 left-0 w-full p-4 z-10">
                             <h3 className="text-lg font-bold text-white"
