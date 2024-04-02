f0c065c6289d018fb19a3f9d99f664e994fadf52
WilliamBerne95 <william.berne@outlook.com>
2024-04-02 02:15:43 +0200

    ml2

--- src/app/MentionsLegales/page.js
+++ src/app/MentionsLegales/page.js
@@ -11,12 +11,12 @@ const Mentionslegales = () => {
             {/* Legal dispositions section */}
             <p>
                 Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004
-                pour la Confiance dans l'économie numérique, dite L.C.E.N., il est porté à la connaissance des
-                utilisateurs et visiteurs, ci-après l'"Utilisateur", du site https://www.eliawu.fr, ci-après le "Site", les
+                pour la Confiance dans l{'\''}économie numérique, dite L.C.E.N., il est porté à la connaissance des
+                utilisateurs et visiteurs, ci-après l{'\''}"Utilisateur", du site https://www.eliawu.fr, ci-après le "Site", les
                 présentes mentions légales.
             </p>
             <p>
-                La connexion et la navigation sur le Site par l'Utilisateur implique acceptation intégrale et sans
+                La connexion et la navigation sur le Site par l{'\''}Utilisateur implique acceptation intégrale et sans
                 réserve des présentes mentions légales.
             </p>
             <p>
@@ -24,17 +24,17 @@ const Mentionslegales = () => {
             </p>
 
             {/* Editor section */}
-            <h2 className="text-2xl font-bold mt-6 mb-4">ARTICLE 1 - L'ÉDITEUR</h2>
+            <h2 className="text-2xl font-bold mt-6 mb-4">ARTICLE 1 - L{'\''}ÉDITEUR</h2>
             <p>
-                L'édition et la direction de la publication du Site est assurée par William Berne, domicilié
-                10 rue des Tournelles, 78510 TRIEL-SUR-SEINE, dont le numéro de téléphone est 07 86 13 97 16, et l'adresse
-                e-mail william.berne@outlook.com. Ci-après "l'Éditeur".
+                L{'\''}édition et la direction de la publication du Site est assurée par William Berne, domicilié
+                10 rue des Tournelles, 78510 TRIEL-SUR-SEINE, dont le numéro de téléphone est 07 86 13 97 16, et l{'\''}adresse
+                e-mail william.berne@outlook.com. Ci-après "l{'\''}Éditeur".
             </p>
 
             {/* Hosting section */}
-            <h2 className="text-2xl font-bold mt-6 mb-4">ARTICLE 2 - L'HÉBERGEUR</h2>
+            <h2 className="text-2xl font-bold mt-6 mb-4">ARTICLE 2 - L{'\''}HÉBERGEUR</h2>
             <p>
-                L'hébergeur du Site est la société OVH Cloud, dont le siège social est situé au 2 rue Kellermann,
+                L{'\''}hébergeur du Site est la société OVH Cloud, dont le siège social est situé au 2 rue Kellermann,
                 59100 Roubaix, avec le numéro de téléphone : 09 72 10 10 07.
             </p>
 
@@ -45,7 +45,7 @@ const Mentionslegales = () => {
                 programmée ou non et pouvant découler d'une nécessité de maintenance.
             </p>
             <p>
-                En cas de modification, interruption ou suspension du Site, l'Éditeur ne saurait être tenu responsable.
+                En cas de modification, interruption ou suspension du Site, l{'\''}Éditeur ne saurait être tenu responsable.
             </p>
 
             {/* Data collection section */}
@@ -56,7 +56,7 @@ const Mentionslegales = () => {
             </p>
             <p>
                 Toute utilisation, reproduction, diffusion, commercialisation, modification de toute ou partie du
-                site, sans autorisation de l'Éditeur est prohibée et pourra entraîner des actions et poursuites
+                site, sans autorisation de l{'\''}Éditeur est prohibée et pourra entraîner des actions et poursuites
                 judiciaires telles que notamment prévues par le Code de la propriété intellectuelle et le Code civil.
             </p>
 
